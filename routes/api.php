ec5b2d8ce03d19f22410e16a636f8a519e2d4632
Faisal <ffalowaini@gmail.com>
2021-12-21 21:12:45 +0300

    add project files

---
+++ routes/api.php
@@ -0,0 +1,59 @@
+<?php
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// Route::get('/products', [ProductController::class, 'index']);
+
+// Route::get('/products', function (){
+//     return Product::all();
+// });
+
+// Route::post('/products', [ProductController::class, 'store']);
+
+// Route::post('/products', function (){
+//     return Product::create([
+//         'name' => 'Product One',
+//         'slug' => 'Product-One',
+//         'desc' => 'Product one desc',
+//         'price' => '20',
+
+//     ]);
+// });
+// Route::resource('products', ProductController::class);
+
+
+//***** Public Route *****//
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::get('/products/search/{name}', [ProductController::class, 'search']);
+
+
+//***** Protected Route *****//
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destory']); 
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+});
+
+
+// Route::middleware('auth:sanctum')->get('/user', function () {
+//     Route::get('/products/search/{name}', [ProductController::class, 'search']);
+//     return $request->user();
+// });
